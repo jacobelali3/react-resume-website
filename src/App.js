ef192302d7a658f219c661c81b50ab6f6097fb41
Jacob Elali <jacobelali3@outlook.com>
2021-12-26 03:37:32 +1100

    Content and Updates
    Added some content on projects, updated packages

--- src/App.js
+++ src/App.js
@@ -75,14 +75,15 @@ function App() {
         <div className="lg:ml-72 sm:ml-32 md:ml-32 relative py-4 px-4 ">
           <div className="blur opacity-50 absolute -inset-0 bg-blue-200 rounded-full"></div>
           <img
+            alt="jacobElali"
             className=" border-transparent hover:border-2 rounded-full relative lg:w-60 md:w-40 sm:w-40"
             src={"/myprofilepic.jpg"}
           />
         </div>
       </div>
 
-      <div className="flex">
-        <div className="lg:ml-20   mt-2  px-4 text-black  lg:w-1/2 lg:h-1/4 sm:w-screen sm:h-fit">
+      <div className="flex text-slate-800">
+        <div className="lg:ml-20   mt-2  px-4   lg:w-1/2 lg:h-1/4 sm:w-screen sm:h-fit">
           <h2 className=" text-4xl text-left font-bold">About Me</h2>
           <p className=" text-2xl font-sans mt-6 ">
             I am a junior software engineer based in Sydney, Australia. My
@@ -109,13 +110,14 @@ function App() {
           </Suspense>
         </Canvas>
       </div>
-      <div className="flex top-48">
-        <div className="lg:ml-20  float-left   px-4 text-black  lg:w-1/2 lg:h-1/4 sm:w-screen sm:h-fit">
+      <div className="text-slate-800 flex top-48">
+        <div className="lg:ml-20  float-left   px-4   lg:w-1/2 lg:h-1/4 sm:w-screen sm:h-fit">
           <h2 className="text-4xl text-left font-bold">Academia</h2>
           <img
             className="place-items-center w-32 float-right"
             src="utslogo.jpg"
             alt="utslogo"
+            rel="noreferrer"
           ></img>
           <p className="text-2xl mt-6">
             <b>Course: </b>Bachelor of Engineering (Honours) <br></br>{" "}
@@ -129,52 +131,89 @@ function App() {
           </p>
         </div>
       </div>
-      <div className="flex mt-16">
-        <div className="lg:ml-20  float-left   px-4 text-black text-2xl lg:w-1/2 lg:h-1/4 sm:w-screen sm:h-fit">
+      <div className="flex mt-16 text-slate-800">
+        <div className="lg:ml-20  float-left   px-4  text-2xl lg:w-1/2 lg:h-1/4 sm:w-screen sm:h-fit">
           <h2 className="text-4xl text-left font-bold">Projects</h2>
-          <a href="https://github.com/jacobelali3/react-resume-website" target="_blank">
-          <img
-            className="place-items-center w-20 animate-bounce float-right"
-            src="githubLogo.png"
-            alt="Git Hub Logo"
-          ></img>
+          
+          <a
+            href="https://github.com/jacobelali3/Software-Engineering-Studio-2B"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img
+              className="place-items-center mt-4  animate-bounce w-20 float-right"
+              src="githubLogo.png"
+              alt="Git Hub Logo"
+            ></img>
           </a>
-          <p className="text-slate-800  font-sans mt-6 ">
-            <b>This Website</b><br></br><br></br> This website is made with React, Three.js
-            and TailwindCSS. Three.js is used to render the cute wallE model you
-            see above and Tailwind is used for styling. This website was built to serve as my portfolio website. I intially built this
-            website using the new REMIX framework, which can be found<a className='text-bold text-blue-500'href="https://github.com/jacobelali3/REMIXresumeWebsite" target="_blank"> here</a>, but there were alot of complications with the 3d model.
+          <p className="  font-sans  mt-10 ">
+            <b>UTS HELPS Prototype</b>
+            <br></br>
+            <br></br> Collaborated on a prototype for the outdated UTS HELPS
+            website that handled student appointment booking. The website was
+            built with django, sqlite, JS and html. In addition to booking, the
+            system had authentication, chat, email and various admin
+            functionality.
           </p>
-          <a href="https://github.com/jacobelali3/react-resume-website" target="_blank">
-          <img
-            className="place-items-center animate-bounce w-20 float-right"
-            src="githubLogo.png"
-            alt="Git Hub Logo"
-          ></img>
+          <a
+            href="https://github.com/jacobelali3/2021-SES3B-Team4"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img
+              className="place-items-center mt-10 animate-bounce w-20 float-right"
+              src="githubLogo.png"
+              alt="Git Hub Logo"
+            ></img>
           </a>
-          <p className="  font-sans mt-6 ">
-            <b>This Website</b><br></br><br></br> This website is build with React, Three.js
-            and TailwindCSS. Three.js is used to render the cute wallE model you
-            see above and Tailwind is used for styling. This website is built
-            from scratch to serve as my portfolio website. I intially built this
-            website using the new REMIX framework, which can be found<a className='text-bold text-blue-500'href="https://github.com/jacobelali3/REMIXresumeWebsite" target="_blank"> here</a>, but there were alot of complications with the 3d model.
+          <p className=" font-sans mt-10 ">
+            <b>COVID-19 App</b>
+            <br></br>
+            <br></br> Collaborated on an application that was developed in
+            response to the Covid-19 pandemic. The main feature consisted of a
+            chatbot that predicted if a patient had covid. In addition; the app
+            provided statistics, news, visual models and client-to-doctor
+            communication functionality. It was built with React, Material UI
+            and Flask.
           </p>
-          <a href="https://github.com/jacobelali3/react-resume-website" target="_blank">
-          <img
-            className="place-items-center  animate-bounce w-20 float-right"
-            src="githubLogo.png"
-            alt="Git Hub Logo"
-          ></img>
+          <a
+            href="https://github.com/jacobelali3/react-resume-website"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="place-items-center mt-10 w-20 animate-bounce float-right"
+              src="githubLogo.png"
+              alt="Git Hub Logo"
+            ></img>
           </a>
-          <p className="  font-sans mt-6 ">
-            <b>This Website</b><br></br><br></br> This website is build with React, Three.js
-            and TailwindCSS. Three.js is used to render the cute wallE model you
-            see above and Tailwind is used for styling. This website is built
-            from scratch to serve as my portfolio website. I intially built this
-            website using the new REMIX framework, which can be found<a className='text-bold text-blue-500'href="https://github.com/jacobelali3/REMIXresumeWebsite" target="_blank"> here</a>, but there were alot of complications with the 3d model.
+          <p className="  font-sans mt-10 ">
+            <b>This Website</b>
+            <br></br>
+            <br></br> This website is made with React, Three.js and TailwindCSS.
+            Three.js is used to render the cute wallE model you see above and
+            Tailwind is used for styling. This website was built to serve as my
+            portfolio website. I intially built this website using the new REMIX
+            framework, which can be found
+            <a
+              className="text-bold text-blue-500"
+              href="https://github.com/jacobelali3/REMIXresumeWebsite"
+              rel="noreferrer"
+              target="_blank"
+            >
+              {" "}
+              here
+            </a>
+            , but there were alot of complications with the 3d model.
           </p>
-        
-        
+          <p className="font-semibold mt-10 ">Other Projects</p>
+          <ul className="text-slate-800  font-sans mt-6 list-disc"> 
+            
+            
+            <li className="hover:text-blue-500"><a href="https://github.com/jacobelali3/CprogrammingUTS">Simple interest calculator using <b>C</b>.</a></li>
+            <li className="hover:text-blue-500"><a href="https://github.com/jacobelali3/CProg">Encryption and compression using <b>C</b></a>.</li>
+            <li className="hover:text-blue-500"><a href="https://github.com/jacobelali3/SES-1B-Group-3">Android health app with map and heart rate monitor using <b>Java</b>.</a></li>
+          </ul>
         </div>
       </div>
     </>
