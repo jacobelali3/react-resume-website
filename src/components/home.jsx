ef7beed3c0f40bfcdc3222ac88f6517d70b14d22
Jacob Elali <jacobelali3@outlook.com>
2022-01-26 23:57:41 +1100

    video fix

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,7 +5,7 @@ function homepage() {
   return (
     <div>
     <div className="w-full h-full top-0 bottom-0 absolute overflow-hidden  " >
-      <video autoPlay loop muted className="object-cover w-auto h-auto min-w-full min-h-full -z-50 ">
+      <video playsInline autoPlay loop muted className="object-cover w-auto h-auto min-w-full min-h-full -z-50 ">
         <source src={Video} type="video/mp4"></source>
       </video>
      
