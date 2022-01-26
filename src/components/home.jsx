b21c4ad2e427dc86c7dd7d57808de61061775d98
Jacob Elali <jacobelali3@outlook.com>
2022-01-27 00:46:41 +1100

    Fixing css issue on homepage

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,7 +4,7 @@ import Video from "./Video/Code-Right3.mp4"
 function homepage() {
   return (
     <div>
-    <div className="w-full h-full top-0 bottom-0 absolute overflow-hidden  " >
+    <div className="w-full h-full top-0 bottom-0 absolute overflow-hidden object-cover min-h-fill min-w-full " >
       <video playsInline autoPlay loop muted className="object-cover w-full h-full min-w-full min-h-full -z-50 ">
         <source src={Video} type="video/mp4"></source>
       </video>
@@ -15,7 +15,7 @@ function homepage() {
     </div>
 
     <div  className=" h-22 mt-48   flex  justify-center mx-12">
-      <div className="px-6 py-6  bg-slate-800 rounded-lg animate-beat ">
+      <div className="px-6 py-6 relative bg-slate-800 rounded-lg animate-beat ">
         <h1 className="text-2xl text-white text-center">Welcome,<br></br>I'm a software engineer with a passion for technology!</h1>
         <p className="pt-4 text-white text-center">Check me out!</p>
       </div>
